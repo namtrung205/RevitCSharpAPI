af79552b18a0ac38522f89fe8ebe5bba8372d1ec
namtrung205 <36217880+namtrung205@users.noreply.github.com>
2019-03-21 23:14:06 +0700

    Add files via upload

---
+++ ShowDialog/Source/ShowDialog/InputNumber.Designer.cs
@@ -0,0 +1,121 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: NAMTRUNG205
+ * Date: 3/5/2019
+ * Time: 9:48 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace ShowDialog
+{
+	partial class InputNumber
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.TXT_Box = new System.Windows.Forms.TextBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.PickAD_Btn = new System.Windows.Forms.Button();
+			this.Pick3P_Btn = new System.Windows.Forms.Button();
+			this.OK_Btn = new System.Windows.Forms.Button();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// TXT_Box
+			// 
+			this.TXT_Box.Location = new System.Drawing.Point(6, 19);
+			this.TXT_Box.Name = "TXT_Box";
+			this.TXT_Box.Size = new System.Drawing.Size(72, 20);
+			this.TXT_Box.TabIndex = 0;
+			this.TXT_Box.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TXT_Box_KeyPress);
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.PickAD_Btn);
+			this.groupBox1.Controls.Add(this.Pick3P_Btn);
+			this.groupBox1.Controls.Add(this.TXT_Box);
+			this.groupBox1.Location = new System.Drawing.Point(12, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(227, 47);
+			this.groupBox1.TabIndex = 2;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Text To Display";
+			this.groupBox1.UseCompatibleTextRendering = true;
+			// 
+			// PickAD_Btn
+			// 
+			this.PickAD_Btn.Location = new System.Drawing.Point(84, 16);
+			this.PickAD_Btn.Name = "PickAD_Btn";
+			this.PickAD_Btn.Size = new System.Drawing.Size(29, 23);
+			this.PickAD_Btn.TabIndex = 2;
+			this.PickAD_Btn.Text = "PA";
+			this.PickAD_Btn.UseCompatibleTextRendering = true;
+			this.PickAD_Btn.UseVisualStyleBackColor = true;
+			this.PickAD_Btn.Click += new System.EventHandler(this.PickAD_BtnClick);
+			// 
+			// Pick3P_Btn
+			// 
+			this.Pick3P_Btn.Location = new System.Drawing.Point(117, 16);
+			this.Pick3P_Btn.Name = "Pick3P_Btn";
+			this.Pick3P_Btn.Size = new System.Drawing.Size(29, 23);
+			this.Pick3P_Btn.TabIndex = 1;
+			this.Pick3P_Btn.Text = "3P";
+			this.Pick3P_Btn.UseCompatibleTextRendering = true;
+			this.Pick3P_Btn.UseVisualStyleBackColor = true;
+			this.Pick3P_Btn.Click += new System.EventHandler(this.Pick3P_BtnClick);
+			// 
+			// OK_Btn
+			// 
+			this.OK_Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.OK_Btn.Location = new System.Drawing.Point(255, 17);
+			this.OK_Btn.Name = "OK_Btn";
+			this.OK_Btn.Size = new System.Drawing.Size(75, 42);
+			this.OK_Btn.TabIndex = 5;
+			this.OK_Btn.Text = "OK";
+			this.OK_Btn.UseCompatibleTextRendering = true;
+			this.OK_Btn.Click += new System.EventHandler(this.OK_BtnClick);
+			// 
+			// InputNumber
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(344, 73);
+			this.Controls.Add(this.OK_Btn);
+			this.Controls.Add(this.groupBox1);
+			this.Name = "InputNumber";
+			this.Text = "Input Number";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Button PickAD_Btn;
+		private System.Windows.Forms.Button Pick3P_Btn;
+		private System.Windows.Forms.Button OK_Btn;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.TextBox TXT_Box;
+		
+	}
+}
---
+++ WorkingWithFace/Source/WorkingWithFace/ThisApplication.cs
@@ -0,0 +1,273 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: NAMTRUNG205
+ * Date: 3/17/2019
+ * Time: 1:24 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WorkingWithFace
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.DB.Macros.AddInId("DE5D6781-2A6E-4DB8-B433-0B73DE790F9E")]
+	public partial class ThisApplication
+	{
+		private void Module_Startup(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Module_Shutdown(object sender, EventArgs e)
+		{
+
+		}
+
+		#region Revit Macros generated code
+		private void InternalStartup()
+		{
+			this.Startup += new System.EventHandler(Module_Startup);
+			this.Shutdown += new System.EventHandler(Module_Shutdown);
+		}
+		#endregion
+		
+		
+		
+		public void getInfo()
+		{
+			// set active document
+			UIDocument uiDoc = this.ActiveUIDocument;
+			Document doc = uiDoc.Document;
+			
+			// prompt select face 
+			
+			Reference myRef = uiDoc.Selection.PickObject(ObjectType.Face);
+			
+			// Get element by pickface
+			Element e = doc.GetElement(myRef) as Element;
+			
+
+			//Get GeoObject from element;
+			GeometryObject myGeoObj =  e.GetGeometryObjectFromReference(myRef) as Face;
+			
+			//Get face from element Object:
+			Face myPickedFace = myGeoObj as Face;
+			
+			// show Info of Face
+			UV myPoint = new UV(0,0);
+			
+			BoundingBoxUV myBoudingBox = myPickedFace.GetBoundingBox();
+			
+			TaskDialog.Show("Abc", "\nMax boundingbox : " + myBoudingBox.Max.U + ", " + myBoudingBox.Max.V);
+
+			
+			// Get normal vector
+			XYZ myXYZpoint = myPickedFace.ComputeNormal(myPoint);
+			TaskDialog.Show("Normal Point: ", XYZtoString(myXYZpoint));
+			
+			// Get BoundingBox
+			BoundingBoxUV faceUVBound = myPickedFace.GetBoundingBox();
+			TaskDialog.Show("abc","max: " + faceUVBound.Max.ToString() + Environment.NewLine 
+			                + "min: " + faceUVBound.Min.ToString());
+			
+			TaskDialog.Show("abc","the face was picked has area: "+ myPickedFace.Area.ToString());
+			
+		}
+		
+		public void paintSurface()
+	{
+		// set active document
+		UIDocument uiDoc = this.ActiveUIDocument;
+		Document doc = uiDoc.Document;
+		
+		// prompt select face 
+		
+		List<Reference> myLisrRef = uiDoc.Selection.PickObjects(ObjectType.Face) as List<Reference>;
+		
+		
+		FilteredElementCollector fec = new FilteredElementCollector(doc).OfClass(typeof(Material));
+		
+		Material materialElem = fec.
+			Cast<Material>().
+			First<Material>( myMaterial => myMaterial.Name == "Default Form" );
+		
+		ElementId myMaterialEleId = materialElem.Id;
+		
+		using (Transaction trans = new Transaction(doc, "Paint surface" )) 
+		{
+			trans.Start();
+			foreach (Reference myRef in myLisrRef) 
+			{
+				Element E = doc.GetElement(myRef);
+				
+				GeometryObject myGeoObj = E.GetGeometryObjectFromReference(myRef);
+				
+				Face myPickedFace = myGeoObj as Face;
+				
+				doc.Paint(E.Id, myPickedFace, myMaterialEleId);
+			}
+			trans.Commit();
+		}	
+	
+	}
+	
+		
+	
+		public void unPaintSurface()
+	{
+		// set active document
+		UIDocument uiDoc = this.ActiveUIDocument;
+		Document doc = uiDoc.Document;
+		
+		// prompt select face 
+		
+		List<Reference> myLisrRef = uiDoc.Selection.PickObjects(ObjectType.Face) as List<Reference>;
+		
+		
+		FilteredElementCollector fec = new FilteredElementCollector(doc).OfClass(typeof(Material));
+		
+		Material materialElem = fec.
+			Cast<Material>().
+			First<Material>( myMaterial => myMaterial.Name == "Default Form" );
+		
+		ElementId myMaterialEleId = materialElem.Id;
+		
+		using (Transaction trans = new Transaction(doc, "Paint surface" )) 
+		{
+			trans.Start();
+			foreach (Reference myRef in myLisrRef) 
+			{
+				Element E = doc.GetElement(myRef);
+				
+				GeometryObject myGeoObj = E.GetGeometryObjectFromReference(myRef);
+				
+				Face myPickedFace = myGeoObj as Face;
+				
+				doc.RemovePaint(E.Id, myPickedFace);
+
+			}
+			trans.Commit();
+		}
+			
+		
+	}
+	
+		
+
+		public void paintWalls()
+		{
+			UIDocument uiDoc = this.ActiveUIDocument;
+			Document doc = uiDoc.Document;
+			
+			
+			//filtered
+			
+			List<string> myFil = new List<string>(){"Wall"};
+			
+			// Select multiple Wall
+			List<Reference> myListRef = uiDoc.Selection.PickObjects(ObjectType.Element, 
+			                 new FilterByNameElementType(myFil), "Select Walls") as List<Reference>;
+			
+			
+			//Get Material Element
+			
+						
+			FilteredElementCollector fec = new FilteredElementCollector(doc).OfClass(typeof(Material));
+    		
+    		Material materialElem = fec.
+    			Cast<Material>().
+    			First<Material>( myMaterial => myMaterial.Name == "Default Form" );
+			
+			ElementId myMaterialEleId = materialElem.Id;
+			
+			
+			
+			foreach (Reference myRef in myListRef) 
+			{
+				Element myElem = doc.GetElement(myRef);
+				Wall myWall = myElem as Wall;
+				
+				paintFaceOfWall(myWall, myMaterialEleId);
+			}
+
+		}
+		
+		private void paintFaceOfWall(Wall myWall, ElementId myMatId)
+		{
+			UIDocument uiDoc = this.ActiveUIDocument;
+			Document doc = uiDoc.Document;
+			
+			GeometryElement geometryElement = myWall.get_Geometry(new Options());
+			
+			using (Transaction myTrans = new Transaction(doc, "fil face of wall and Paint")) 
+			{
+			
+				myTrans.Start();
+				UV myPoint = new UV(0,0);
+
+				foreach (GeometryObject geometryObject in geometryElement)
+			    {
+			        if (geometryObject is Solid)
+			        {
+			            Solid solid = geometryObject as Solid;
+			            XYZ myNormVec = new XYZ();
+			            foreach (Face myFace in solid.Faces)
+			            {
+			            	myNormVec = myFace.ComputeNormal(myPoint);
+			            	if (Math.Round(Math.Abs(myNormVec.Z), 1) != 1.0)
+							{
+			            		doc.Paint(myWall.Id, myFace, myMatId);
+							}
+			            }
+			        }
+			    }
+				myTrans.Commit();
+			}
+		
+		}
+		
+		
+		private string XYZtoString(XYZ myXYZ)
+		{
+			return string.Format("X: {0}, Y: {1}, Z: {2}", Math.Round(myXYZ.X,2), Math.Round(myXYZ.Y, 2), Math.Round(myXYZ.Z,2));
+		
+		}
+	}
+	
+		public class FilterByNameElementType : ISelectionFilter
+	{
+		
+		//Cac bien thanh vien
+		List<string> myListNameFilter = new List<string>();
+		
+		// Bo khoi dung
+		public FilterByNameElementType(List<string> myListName){
+		myListNameFilter = myListName;
+		}
+		
+		public bool AllowElement(Element e)
+		{
+			string typeE = e.GetType().Name.ToString();
+
+			if(this.myListNameFilter.Contains(typeE))
+			{
+			return true;
+			}
+			
+			return false;
+		}
+	
+		public bool AllowReference(Reference r, XYZ myXYZ) 
+		{
+				return true;
+		}
+	
+	}
+	
+}
